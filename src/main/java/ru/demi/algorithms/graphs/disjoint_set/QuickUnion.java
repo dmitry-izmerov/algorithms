843d0a4ec0a57f9607c72bf700f01aee16e9f8bf
Dmitrii_Izmerov <idd90i@gmail.com>
2021-09-04 12:28:17 +0300

    Graphs - Disjoint set - Quick union

---
+++ src/main/java/ru/demi/algorithms/graphs/disjoint_set/QuickUnion.java
@@ -0,0 +1,32 @@
+package ru.demi.algorithms.graphs.disjoint_set;
+
+public class QuickUnion {
+    private final int[] roots;
+
+    public QuickUnion(int length) {
+        roots = new int[length];
+        for (int i = 0; i < roots.length; i++) {
+            roots[i] = i;
+        }
+    }
+
+    public int findRoot(int vertex) {
+        var root = vertex;
+        while (root != roots[root]) {
+            root = roots[root];
+        }
+        return root;
+    }
+
+    public void union(int vertexA, int vertexB) {
+        var rootA = findRoot(vertexA);
+        var rootB = findRoot(vertexB);
+        if (rootA != rootB) {
+            roots[rootB] = rootA;
+        }
+    }
+
+    public boolean isConnected(int vertexA, int vertexB) {
+        return findRoot(vertexA) == findRoot(vertexB);
+    }
+}
