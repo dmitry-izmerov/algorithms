66e969c2a3577f9fbb9c16ad10f62ceb584e95c6
Dmitrii_Izmerov <idd90i@gmail.com>
2021-09-04 15:07:35 +0300

    Graphs - Disjoint set - Quick union with path compression

---
+++ src/main/java/ru/demi/algorithms/graphs/disjoint_set/QuickUnionWithPathCompression.java
@@ -0,0 +1,16 @@
+package ru.demi.algorithms.graphs.disjoint_set;
+
+public class QuickUnionWithPathCompression extends QuickUnion {
+    public QuickUnionWithPathCompression(int length) {
+        super(length);
+    }
+
+    @Override
+    public int findRoot(int vertex) {
+        if (vertex == roots[vertex]) {
+            return vertex;
+        }
+        roots[vertex] = findRoot(roots[vertex]);
+        return roots[vertex];
+    }
+}
