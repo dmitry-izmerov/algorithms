ebb74e04aca4cf3c474207b48bf0217d0bcf0765
Dmitrii_Izmerov <idd90i@gmail.com>
2021-09-04 14:50:09 +0300

    tests for the rest of disjoint sets

--- src/test/java/ru/demi/algorithms/graphs/disjoint_set/QuickUnionTest.java
+++ src/test/java/ru/demi/algorithms/graphs/disjoint_set/QuickUnionTest.java
@@ -1,9 +1,9 @@
 package ru.demi.algorithms.graphs.disjoint_set;
 
-class QuickFindTest extends AbstractQuickFindTest {
+class QuickUnionTest extends AbstractQuickFindTest {
 
     @Override
     DisjointSet getDisjointSet(int length) {
-        return new QuickFind(length);
+        return new QuickUnion(length);
     }
 }
